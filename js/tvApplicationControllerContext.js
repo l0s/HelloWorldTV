e8b88a16b1a50a06b822897b286e4dbd9a841305
Carlos Macasaet <github@carlos-m.net>
2015-09-19 23:57:04 -0700

    play trailer

--- js/tvApplicationControllerContext.js
+++ js/tvApplicationControllerContext.js
@@ -1,24 +1,33 @@
-function getDocument( url )
+function playVideo()
 {
-  var request = new XMLHttpRequest();
-  request.responseType = "document";
-  var responseHandler = function()
-  {
-    console.log( "Handling document" );
-    navigationDocument.pushDocument( request.responseXML );
-  };
-  request.addEventListener( "load", responseHandler, false );
+	var mediaItem = new MediaItem( "video", "http://movietrailers.apple.com/movies/sony_pictures/hoteltransylvania2/hoteltransylvania2-tlr1_720p.mov" );
 
-  request.open( "GET", url, true );
-  request.send();
-  return request;
+	var playlist = new Playlist();
+	playlist.push( mediaItem );
+
+	var player = new Player();
+	player.playlist = playlist;
+	player.present();
 }
 
 App.onLaunch = function( options )
 {
   console.log( 'App launching' );
-  var url = "https://l0s.github.io/HelloWorldTV/template/index.tvml";
-  getDocument( url );
+  var request = new XMLHttpRequest();
+  request.responseType = "document";
+  var responseHandler = function()
+  {
+    console.log( "Handling document" );
+    // navigationDocument.pushDocument( request.responseXML );
+    var parser = new DOMParser();
+    var document = parser.parseFromString( request.responseXML, "application/xml" );
+    document.addEventListener( "select", playVideo, false );
+    navigationDocument.pushDocument( document );
+  };
+  request.addEventListener( "load", responseHandler, false );
+
+  request.open( "GET", "https://l0s.github.io/HelloWorldTV/template/index.tvml", true );
+  request.send();
 };
 
 App.onExit = function()
